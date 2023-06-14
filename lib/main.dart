7942b6a918517e26012485bab1a1ce7558a9df09
chioma <chiomag58@gmail.com>
2023-06-14 20:37:54 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'search.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherApp(),
+    ),
+  );
+}
